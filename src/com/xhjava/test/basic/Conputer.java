2599d65305676c85223615294ac7cba7c5a15105
xiahui1307080229 <xiahui9999@qq.com>
2017-07-25 18:55:44 +0800

    commit computer class file

---
+++ src/com/xhjava/test/basic/Conputer.java
@@ -0,0 +1,20 @@
+package com.xhjava.test.basic;
+
+public class Conputer {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1 + 2);
+		
+		System.out.println(add(1, 2));
+	}
+	
+	public static int add(int a, int b){
+		return a + b;
+	}
+	
+	public int sub(int a, int b){
+		return a - b;
+	}
+
+}
