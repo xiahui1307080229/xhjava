00c0f1b5e49919e0c8006773af7434ee04618143
xiahui1307080229 <xiahui9999@qq.com>
2017-07-26 14:50:31 +0800

    dgdgd

---
+++ src/com/xhjava/test/basic/UseVolatile.java
@@ -0,0 +1,28 @@
+package com.xhjava.test.basic;
+
+public class UseVolatile {
+	 private volatile boolean active;
+	    public void run()
+	    {
+	        active = true;
+	        while (active) 
+	        {
+	            System.out.println("go on!");
+	        }
+	    }
+	    public void stop()
+	    {
+	        active = false;
+	    }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UseVolatile UV = new UseVolatile();
+	//	UV.run();
+	//	UV.stop();
+		UV.stop();
+		UV.run();
+	}
+
+}
