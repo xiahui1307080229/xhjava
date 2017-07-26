50b89d89191acefa6b90ad773f956cf9c95b2384
xiahui1307080229 <xiahui9999@qq.com>
2017-07-26 09:31:14 +0800

    DataLook.java

---
+++ src/com/xhjava/test/basic/DataLook.java
@@ -0,0 +1,24 @@
+package com.xhjava.test.basic;
+
+public class DataLook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 8;
+		int y = 8;
+		if(x==y)
+		{
+			System.out.println("这是不对的！");
+		}
+		if(true)
+		{
+			System.out.println("这货不是假的！");
+		}
+		if(2016 % 2 == 0)
+		{
+			System.out.println("好年头");
+		}
+		
+	}
+
+}
