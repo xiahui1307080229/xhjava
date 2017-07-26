a7bfea9c81be6761bf57a4c3b3ad84b9e9c1d238
xiahui1307080229 <xiahui9999@qq.com>
2017-07-26 17:54:30 +0800

    Packing.java

---
+++ src/com/xhjava/test/basic/Packing.java
@@ -0,0 +1,31 @@
+package com.xhjava.test.basic;
+
+public class Packing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i1 = 200;   //×°Ïä
+		Integer i2 = 200;	
+		int j1 = 100;		//²ğÏä
+		int j2 = 100;
+		
+		if(i1 == i2)
+		{
+			System.out.println("true");
+		} 
+		else 
+		{
+			System.out.println("false");
+		}
+		if(j1 == j2)
+		{
+			System.out.println("ture");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+	}
+
+}
