193aae0a26a7c2d902450112354df8aff796093b
xiahui1307080229 <xiahui9999@qq.com>
2017-07-26 09:19:22 +0800

    MyDemo.java

---
+++ src/com/xhjava/test/basic/MyDemo.java
@@ -0,0 +1,13 @@
+package com.xhjava.test.basic;
+
+public class MyDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "haha";
+		int x = 007;
+		str = str + x;
+		System.out.println("str = " + str);
+	}
+
+}
