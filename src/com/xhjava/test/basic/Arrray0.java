17e8f987b040fdd0115a5fe75ba37c2f1590beb9
xiahui1307080229 <xiahui9999@qq.com>
2017-07-26 09:12:41 +0800

    Arrray0

--- src/com/xhjava/test/basic/Arrray0.java
+++ src/com/xhjava/test/basic/Arrray0.java
@@ -4,7 +4,16 @@ public class Arrray0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int score[] = null;
+		score = new int[3];
+		for(int x = 0; x < 3; x++)
+		{
+			score[x] = x * 2 + 1;
+		}
+		for(int x = 0; x < score.length ; x++)
+		{
+			System.out.println("score["+x+"] =" + score[x]);
+		}
 	}
 
 }
