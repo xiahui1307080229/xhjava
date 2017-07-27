d631a77778e543bca02bdf1b7d86889443fb7199
xiahui1307080229 <xiahui9999@qq.com>
2017-07-27 09:23:16 +0800

    sas

--- src/com/xhjava/test/basic/Packing.java
+++ src/com/xhjava/test/basic/Packing.java
@@ -5,10 +5,10 @@ public class Packing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer i1 = 
-				new Integer(200);   //×°Ïä
-		Integer i2 = 200;	
-		int j1 = 100;		//²ğÏä
-		int j2 = 100;
+				new Integer(100);   //×°Ïä
+		Integer i2 = 100;	
+		int j1 = 200;		//²ğÏä
+		int j2 = 200;
 		
 		if(i1 == i2)
 		{
