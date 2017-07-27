49b3bef3dc0f82deebe6070932c71a96b16d56e4
xiahui1307080229 <xiahui9999@qq.com>
2017-07-27 08:40:54 +0800

    rwerwe

--- src/com/xhjava/test/basic/Packing.java
+++ src/com/xhjava/test/basic/Packing.java
@@ -4,7 +4,8 @@ public class Packing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Integer i1 = 200;   //×°Ïä
+		Integer i1 = 
+				new Integer(200);   //×°Ïä
 		Integer i2 = 200;	
 		int j1 = 100;		//²ğÏä
 		int j2 = 100;
