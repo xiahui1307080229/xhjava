aa9bf78cd680364e0705280c26ce8147e4033c2d
xiahui1307080229 <xiahui9999@qq.com>
2017-07-28 11:35:31 +0800

    fgdsffsd

--- src/com/xhjava/test/basic/IoCol.java
+++ src/com/xhjava/test/basic/IoCol.java
@@ -3,12 +3,17 @@ package com.xhjava.test.basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+
 
 public class IoCol {
 
 	public static void main(String[] args)throws IOException
 	{
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String sc = scan.next();
+		System.out.println(" ‰»ÎSC÷µ£∫" + sc);
 		char c;
 		BufferedReader br = new BufferedReader(new 
                 InputStreamReader(System.in));
