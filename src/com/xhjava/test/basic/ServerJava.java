689822083c00eacc268de2216f305a41b9b0d584
xiahui1307080229 <xiahui9999@qq.com>
2017-07-31 10:06:22 +0800

    rwerwerwe

---
+++ src/com/xhjava/test/basic/ServerJava.java
@@ -0,0 +1,58 @@
+package com.xhjava.test.basic;
+
+import java.net.*;
+import java.io.*;
+ 
+public class ServerJava extends Thread
+{
+   private ServerSocket serverSocket;
+   
+   public ServerJava(int port) throws IOException
+   {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setSoTimeout(10000);
+   }
+ 
+   public void run()
+   {
+      while(true)
+      {
+         try
+         {
+            System.out.println("Waiting for client on port " +
+            serverSocket.getLocalPort() + "...");
+            Socket server = serverSocket.accept();
+            System.out.println("Just connected to "
+                  + server.getRemoteSocketAddress());
+            DataInputStream in =
+                  new DataInputStream(server.getInputStream());
+            System.out.println(in.readUTF());
+            DataOutputStream out =
+                 new DataOutputStream(server.getOutputStream());
+            out.writeUTF("Thank you for connecting to "
+              + server.getLocalSocketAddress() + "\nGoodbye!");
+            server.close();
+         }catch(SocketTimeoutException s)
+         {
+            System.out.println("Socket timed out!");
+            break;
+         }catch(IOException e)
+         {
+            e.printStackTrace();
+            break;
+         }
+      }
+   }
+   public static void main(String [] args)
+   {
+      int port = 8888;
+      try
+      {
+         Thread t = new ServerJava(port);
+         t.start();
+      }catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
